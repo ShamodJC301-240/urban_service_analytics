a947dccd75a1c3666e0751770c11c74cfe1b7b88
ShamodJC301-240 <92445690+ShamodJC301-240@users.noreply.github.com>
2026-06-03 20:21:24 -0400

    Testing sql queries and running through terminal

--- sql/kpi_queries.sql
+++ sql/kpi_queries.sql
@@ -3,3 +3,34 @@
 -- Open vs closed ratio
 -- Top 5 complaint types
 -- Daily request trends
+
+-- Getting the number of records from our api pull
+SELECT COUNT(*) AS total_content 
+FROM stg_311_requests;
+
+-- Selecting records for inspection pull (10) records
+SELECT *
+FROM stg_311_requests
+LIMIT 10;
+
+-- Fetching total requests and divying them up by borough
+SELECT borough, COUNT(*) AS total_requests
+FROM stg_311_requests
+GROUP BY borough
+ORDER BY total_requests DESC;
+
+-- Getting top complaints
+SELECT complaint_type, COUNT(*) AS total_requests
+FROM stg_311_requests
+GROUP BY complaint_type
+ORDER BY total_requests DESC
+LIMIT 10;
+
+-- SQL test run
+-- Got this error when running: ERROR:  relation "vw_requests_by_borough" already exists So I am adding a REPLACE to query
+
+CREATE OR REPLACE VIEW vw_requests_by_borough AS
+SELECT borough, COUNT(*) AS total_requests
+FROM stg_311_requests
+GROUP BY borough;
+
